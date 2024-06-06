c3243ec6e3b7f08976f991beadd8464ab16991d3
Astemir <aidamir606@mail.ru>
2024-06-06 16:52:50 +0300

    sushi-7

---
+++ app/src/main/java/com/example/sushidelevery/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.example.sushidelevery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Green10 = Color(0xFF6A8B42)
+val Green20 = Color(0xFF48704A)
+val Green30 = Color(0xD520702E)
+
+val Blue1 = Color(0xD5346083)
+val Blue2 = Color(0xD5024470)
+val Blue3 = Color(0xFF0F4588)
+
+val Green1 = Color(0xD555E057)
+val Green2 = Color(0xD50D9E27)
+val Green3 = Color(0xD5025711)
+
+val Violet1 = Color(0xD5802DBB)
+val Violet2 = Color(0xD56D01B9)
+val Violet3 = Color(0xD5422C94)
+
+val Beige1 = Color(0xD534919C)
+val Beige2 = Color(0xFF469E89)
+val Beige3 = Color(0xD515556F)
+
+val Orange1 = Color(0xD5CCB43A)
+val Orange2 = Color(0xFF868B42)
+val Orange3 = Color(0xD58D6F10)
